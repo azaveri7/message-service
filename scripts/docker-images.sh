42a6283da8bd4afb0a1938410b77d71b01c6ed15
azaveri7 <azaveri7@gmail.com>
2020-11-16 19:31:06 +0530

    few commits

--- scripts/docker-images.sh
+++ scripts/docker-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd ..
 docker build -t message-service:latest .
-docker tag message-service:latest azaveri7/microservices:message-service-1
-docker push azaveri7/microservices:message-service-1
+docker tag message-service:latest azaveri7/microservices:message-service
+docker push azaveri7/microservices:message-service
